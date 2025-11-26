cf580cec12893d3b2faff29147b43b124df33aea
Rohan Anantrao Sabnis <rohansabnis9@gmail.com>
2025-11-26 14:00:59 +0530

    Updated Navbar and  Preview files

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -1,14 +1,24 @@
-import { Avatar, Box } from "@mui/material";
+import { Avatar, Box, IconButton } from "@mui/material";
 import React from "react";
 import webtrixLogo from "../../assets/webtrix-logo.png";
 import { useNavigate } from "react-router-dom";
-const Nav = () => {
-  const navigate = useNavigate()
-  return (
-    <Box>
-      <Avatar src={webtrixLogo} />
+import { PreviewRounded } from "@mui/icons-material";
 
-      {/* <Preview />  */}
+const Nav = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{width: "100%", height: "64px", display: "flex", alignItems: "center", justifyContent: "space-between", px: 2, boxShadow: 1, backgroundColor: "#fff"}} >
+      <Avatar
+        src={webtrixLogo}
+        variant="square"
+        sx={{ cursor: "pointer" }}
+        onClick={() => navigate("/")}
+      />
+
+      <IconButton onClick={() => navigate("/preview")} >
+        <PreviewRounded sx={{fontSize: 32}} />
+      </IconButton>
     </Box>
   );
 };
